bff3f8094fd1b3c9d553082a66c023e6b40fa7ca
Irina1234567 <i.yaskova@volga.rt.ru>
2019-04-08 22:10:54 +0300

    Lesson_4

--- Задание 1.py
+++
@@ -1,4 +0,0 @@
-# Задание 1
-age = input('введите число:')
-sum = int(age)+2
-print(sum)
--- Задание 2.py
+++
@@ -1,8 +0,0 @@
-# Задание 2
-number = int(input('введите число:'))
-while number >= 0 and number <= 10:
-    number = number ** 2
-    print(number)
-    break
-else:
-    print('не верно. ведите числов диапазоне от 0 до 10')
